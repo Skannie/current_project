4e3749df7c93ff9ebd4b4c96b5d1d95c220164eb
Karrs Annie <kannie@ga-f3.msk.21-school.ru>
2022-04-21 20:09:49 +0300

    4 commit CP

--- philo.c
+++ philo.c
@@ -6,7 +6,7 @@
 /*   By: kannie <kannie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 16:48:26 by kannie            #+#    #+#             */
-/*   Updated: 2022/04/20 20:02:48 by kannie           ###   ########.fr       */
+/*   Updated: 2022/04/21 18:48:29 by kannie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,57 +36,57 @@
 // определения смерти философа, смотреть когда в последний раз ел философ
 // относительно времени когда я зашёл в функцию ревизорро.
 
-int	time_to(t_philo *philo)
-{
-	int	time;
+// int	time_to(t_philo *philo)
+// {
+// 	int	time;
 
-	gettimeofday(&philo->current_time, NULL); // начало отчёта времени до смерти
-	time = philo->current_time.tv_usec
-		+ (philo->current_time.tv_sec * 1000000);
-	return (time);
-}
+// 	gettimeofday(&philo->current_time, NULL); // начало отчёта времени до смерти
+// 	time = philo->current_time.tv_usec
+// 		+ (philo->current_time.tv_sec * 1000000);
+// 	return (time);
+// }
 
-void	*function(void *buf)
-{
-	int				time;
-	t_philo			*philo;
-	int				i;
+// void	*function(void *buf)
+// {
+// 	int				time;
+// 	t_philo			*philo;
+// 	int				i;
 
-	i = 1;
-	philo = (t_philo *)buf;
-	while (philo->i == 0)
-	{
-		printf("philo %d: ", philo->number);
-		philo->start = time_to(philo);
-		usleep((philo->time_to_eat)); // время которое философ ел
-		printf("eat up\n");
-		usleep((philo->time_to_sleep) + (i * 1000)); // время сна
-		philo->end = time_to(philo);
-		time = (philo->end - philo->start);
-		printf("time-->%d\n", time);
-		if (time <= philo->time_to_die)
-		{
-			i++;
-			// function ((void *)philo);
-		}
-		else
-		{
-			philo->i++;
-			printf("philosopher -> die\n");
-			printf("Exit_proc\n");
-		}
-	}
-	return (NULL);
-}
+// 	i = 1;
+// 	philo = (t_philo *)buf;
+// 	while (philo->i == 0)
+// 	{
+// 		printf("philo %d: ", philo->number);
+// 		philo->start = time_to(philo);
+// 		usleep((philo->time_to_eat)); // время которое философ ел
+// 		printf("eat up\n");
+// 		usleep((philo->time_to_sleep) + (i * 1000)); // время сна
+// 		philo->end = time_to(philo);
+// 		time = (philo->end - philo->start);
+// 		printf("time-->%d\n", time);
+// 		if (time <= philo->time_to_die)
+// 		{
+// 			i++;
+// 			// function ((void *)philo);
+// 		}
+// 		else
+// 		{
+// 			philo->i++;
+// 			printf("philosopher -> die\n");
+// 			printf("Exit_proc\n");
+// 		}
+// 	}
+// 	return (NULL);
+// }
 
-void	even_philosophers(t_philo *philo)
-{
-	pthread_t	life_philo;
+// void	even_philosophers(t_philo *philo)
+// {
+// 	pthread_t	life_philo;
 
-	printf("chetn\n");
-	pthread_create(&life_philo, NULL, function, philo);
-	pthread_detach(life_philo);
-}
+// 	printf("chetn\n");
+// 	pthread_create(&life_philo, NULL, function, philo);
+// 	pthread_detach(life_philo);
+// }
 
 // void	odd_philosophers(void)
 // {
@@ -95,37 +95,42 @@ void	even_philosophers(t_philo *philo)
 
 void	creat_philo(t_philo *philo)
 {
-	if (philo.number_of_philosophers % 2 == 0)
+	if (philo->number_of_philosophers % 2 == 0)
 	{
-		while (philo.number_of_philosophers-- > 0)
-		{
-			philo.number++;
-			// printf("%d\n", philo.number);
-			if (philo.number == 1)
-				even_philosophers(&philo);
-		}
+		printf("chetn\n");
+		// while (philo->number_of_philosophers-- > 0)
+		// {
+		// 	philo->number++;
+		// 	// printf("%d\n", philo.number);
+		// 	if (philo->number == 1)
+		// 		even_philosophers(&philo);
+		// }
 	}
 	else
 		printf("nechetn\n");
 		// odd_philosophers();
 }
 
-int	values_philo(char *str[], t_philo philo)
+int	values_philo(char *str[], t_philo *philo)
 {
 	int	i;
 
 	i = 0;
-	philo.number_of_philosophers = ft_atoi(str[1]);
-	if (philo.number_of_philosophers < 2)
+	philo->number_of_philosophers = ft_atoi(str[1]);
+	if (philo->number_of_philosophers < 2)
 		i = 1;
-	philo.time_to_die = ft_atoi(str[2]) * 1000;
-	if (philo.time_to_die < 1)
+	philo->time_to_die = ft_atoi(str[2]) * 1000;
+	if (philo->time_to_die < 1)
 		i = 1;
-	philo.time_to_eat = ft_atoi(str[3]) * 1000;
-	if (philo.time_to_die < 1)
+	philo->time_to_eat = ft_atoi(str[3]) * 1000;
+	if (philo->time_to_die < 1)
 		i = 1;
-	philo.time_to_sleep = ft_atoi(str[4]) * 1000;
-	if (philo.time_to_die < 1)
+	philo->time_to_sleep = ft_atoi(str[4]) * 1000;
+	if (philo->time_to_die < 1)
+		i = 1;
+	if (str[5])
+		philo->number_philosopher_must_eat = ft_atoi(str[5]);
+	if (philo->number_philosopher_must_eat < 0)
 		i = 1;
 	return (i);
 }
@@ -136,19 +141,25 @@ int	main(int argc, char **argv)
 	int		i;
 
 	i = 0;
+	philo.number_philosopher_must_eat = 0;
 	philo.i = 0;
-	if (argc == 5)
+	if (argc >= 5 && argc <= 6)
 	{
 		philo.number = 0;
-		i = values_philo(**argv, &philo);
+		i = values_philo(argv, &philo);
+		printf("%d\n", philo.number_of_philosophers);
+		printf("%d\n", philo.time_to_die);
+		printf("%d\n", philo.time_to_eat);
+		printf("%d\n", philo.time_to_sleep);
+		printf("%d\n", philo.number_philosopher_must_eat);
 		if (i == 1)
 		{
 			printf("Error");
 			return (-1);
 		}
-		creat_philo(&philo);
-		while (philo.i == 0)
-			pause();
+		// creat_philo(&philo);
+		// while (philo.i == 0)
+		// 	pause();
 		printf("Exit_philo\n");
 		return (0);
 	}
--- philo.h
+++ philo.h
@@ -6,7 +6,7 @@
 /*   By: kannie <kannie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/08 16:35:29 by kannie            #+#    #+#             */
-/*   Updated: 2022/04/20 18:36:09 by kannie           ###   ########.fr       */
+/*   Updated: 2022/04/21 18:38:58 by kannie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ typedef struct s_philo
 	pthread_mutex_t	mutex;
 	int				i;
 	int				number;
+	int				number_philosopher_must_eat;
 	int				number_of_philosophers;
 	int				number_of_forks;
 	int				time_to_die;
@@ -32,6 +33,12 @@ typedef struct s_philo
 	long			end;
 	struct timeval	current_time;
 }	t_philo;
+
+typedef struct s_waiter
+{
+	
+}	t_waiter;
+
 
 int		ft_atoi(const char *str);
 int		schar_v_int(const char *strok, int i, int minus);
